5db92c70b3c1b0602fb219e2e614d942a37db313
johanalanderos <johana_lansan@hotmail.com>
2015-04-29 14:48:42 -0500

    mi chat en linea

---
+++ SaladeChat/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SaladeChat.SRChat;
+
+namespace SaladeChat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            SRChat.ServicioChatSoapClient ch = new ServicioChatSoapClient();
+            
+            lstSala.Items.Add(txtMensaje.Text);
+            var ans = ch.chatiar(txtMensaje.Text);
+            lstSala.Items.Add(ans);
+        }
+    }
+}
